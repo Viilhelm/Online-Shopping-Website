98cd09654022e669bd24b278a0c69d8eb6178598
Waley <103100819+Viilhelm@users.noreply.github.com>
2023-02-26 23:49:53 +0800

    modified:   web/products/models.py

--- web/products/models.py
+++ web/products/models.py
@@ -2,18 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    productID = models.CharField(max_length=10, primary_key=True)
     productName = models.CharField(max_length=100)
-    price = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+    price = models.DecimalField(default=0, max_digits=7, decimal_places=2)
     ISBN = models.CharField(max_length=17)
     author = models.CharField(max_length=100)
     publisher = models.CharField(max_length=100)
     introduction = models.TextField(max_length=500)
     image = models.ImageField(upload_to='product')
     category = models.ForeignKey('Category', on_delete=models.CASCADE,)
+    avgRating = models.DecimalField(default=0, max_digits=3, decimal_places=2)
 
 
 class Category(models.Model):
-
-    categoryID = models.CharField(max_length=10, primary_key=True)
     categoryName = models.CharField(max_length=50)
