897522e10384b66bf7b6692e3725853636f1a785
Waley <103100819+Viilhelm@users.noreply.github.com>
2023-02-26 23:49:28 +0800

    modified:   web/products/views.py

--- web/products/views.py
+++ web/products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from products.models import Product, Category
 
 # Create your views here.
@@ -10,8 +10,26 @@ class HomePageView(TemplateView):
 class ProductListView(ListView):
     model = Product
     template_name = 'products.html'
+    paginate_by = 6
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['section'] = "allProducts"
+        return context
+class CategoryDetailView(DetailView):
+    model = Category
+    template_name = 'category_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['section'] = "categoryDetail"
+        return context
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['section'] = "productDetail"
         return context
