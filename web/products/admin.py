fb532bbbd066c6d77ad4e7379e8a2a7a6bdb950e
Waley <103100819+Viilhelm@users.noreply.github.com>
2023-03-05 23:37:39 +0800

    Modifed

--- web/products/admin.py
+++ web/products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product,Category,OrderItem
+from .models import Product,Category
 
 # Register your models here.
 @admin.register(Product)
--- web/products/models.py
+++ web/products/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    productID = models.BigAutoField(primary_key=True)
     productName = models.CharField(max_length=100)
     price = models.DecimalField(default=0, max_digits=7, decimal_places=2)
     ISBN = models.CharField(max_length=17)
@@ -17,14 +16,6 @@ class Product(models.Model):
 
 
 class Category(models.Model):
-    categoryID = models.BigAutoField(primary_key=True)
     categoryName = models.CharField(max_length=50)
-    def __str__(self):
-        return self.categoryName
-    
-class OrderItem(models.Model):
-    productID = models.ForeignKey('Product', on_delete=models.CASCADE,)
-    PONumber = models.ForeignKey('customers.Order', on_delete=models.CASCADE,)
-    MyRate = models.DecimalField(default=0, max_digits=3, decimal_places=2)
 
 
