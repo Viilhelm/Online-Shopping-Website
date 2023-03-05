adf2b2880e220f36a5e16adbcc3a845a11efaa95
Waley <103100819+Viilhelm@users.noreply.github.com>
2023-03-05 23:17:08 +0800

    Modified

--- web/customers/urls.py
+++ web/customers/urls.py
@@ -5,7 +5,8 @@ from .forms import LoginForm
 
 urlpatterns = [
     path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
-    path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('profile/', views.CustomerProfileView.as_view(), name='profile'),
+    path('profileAdd/', views.ProfileAddView.as_view(), name='profileAdd'),
    
 
 ]
