a0b8853d0e88a3ffdb80c64e2406c4f7342ae181
Arthur <2484575307@qq.com>
2023-03-06 19:05:10 +0800

    merge

--- web/products/urls.py
+++ web/products/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -7,8 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('home/', views.HomePageView.as_view(), name='home'),
-    path(r'^products/(?P<page>\d+)$', views.ProductListView.as_view(), name='products'),
-    path(r'^products/(?P<category_id>\d+)/(?P<page>\d+)$',views.CategoryView.as_view(), name='category'),
+    re_path(r'^products/(?P<page>\d+)$', views.ProductListView.as_view(), name='products'),
+    re_path(r'^products/(?P<category_id>\d+)/(?P<page>\d+)$',views.CategoryView.as_view(), name='category'),
     path('produtcs/<int:pk>',views.ProductDetailView.as_view(), name='product_detail'),
     
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
