fa5a4095660aade7f99c813529fc0e3193407e7e
Waley <103100819+Viilhelm@users.noreply.github.com>
2023-03-13 12:49:48 +0800

    Modified

--- web/products/urls.py
+++ web/products/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='products'),
-    path('home/', views.HomePageView.as_view(), name='home'),
+    path('home/', views.ProductListView.as_view(), name='home'),
     path('products/', views.ProductListView.as_view(), name='products'),
     path(r'^products/(?P<category_id>\d+)/(?P<page>\d+)$',views.CategoryView.as_view(), name='category'),
     path('produtcs/<int:pk>',views.ProductDetailView.as_view(), name='product_detail'),
