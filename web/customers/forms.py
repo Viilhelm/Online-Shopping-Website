4fcfbe91000c19294f46edb3c725290814b5a509
Waley <103100819+Viilhelm@users.noreply.github.com>
2023-04-16 16:04:20 +0800

    Modified

--- web/customers/forms.py
+++ web/customers/forms.py
@@ -6,6 +6,7 @@ from .models import Customer
 class LoginForm(AuthenticationForm):
     username = UsernameField(widget=forms.TextInput(attrs={'autofocus':'True','class':'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'autocomplete':'current-password','class':'form-control'}))
+    
 
 class CustomerRegistrationForm(UserCreationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'autofocus':'True','class':'form-control'}))
